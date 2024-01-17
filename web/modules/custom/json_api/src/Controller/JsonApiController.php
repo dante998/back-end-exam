07e8167617827d63799c6f92b2d8244718a05319
Mihael Kamenov <114795797+dante998@users.noreply.github.com>
2024-01-17 14:34:29 +0000

    Json api format - working on it.

--- web/modules/custom/json_api/src/Controller/JsonApiController.php
+++ web/modules/custom/json_api/src/Controller/JsonApiController.php
@@ -19,12 +19,20 @@ class JsonApiController extends ControllerBase {
 
     $data = [];
 
+
     foreach ($nodes as $node) {
 
       if ($node->getType() == $course_content_type) {
 
         // Load the referenced instructor node.
         $instructor_reference = $node->get('field_instructor')->entity;
+        // Load the classification of the courses.
+        $subject = $node->get('field_subject')->entity;
+        $level = $node->get('field_level')->entity;
+        $department = $node->get('field_department')->entity;
+        // Load the external resources.
+        $resource = $node->get('field_external_resources')->entity;
+
 
         $instructor_data = [
             'name' => $instructor_reference->getTitle(),
@@ -32,16 +40,26 @@ class JsonApiController extends ControllerBase {
             'contactInfo' => $instructor_reference->get('field_contact_information')->value,
           ];
 
+        $resource_data = [
+            'title' => $resource ? $resource->getTitle() : '',
+            'description' => $resource ? $resource->get('field_resource_description')->value : '',
+
+        ];
+
+
         $data[] = [
           'courseName' => $node->getTitle(),
           'description' => $node->get('field_description')->value,
           'startDate' => $node->get('field_start_date')->value,
           'endDate' => $node->get('field_end_date')->value,
           'instructor' => $instructor_data,
+          'subject' => $subject ? $subject->getName() : '',
+          'level' => $level ? $level->getName() : '',
+          'department' => $department ? $department->getName() : '',
+          'resources' => $resource_data ,
         ];
       }
     }
-
-  return new JsonResponse($data);
+     return new JsonResponse($data);
  }
 }
